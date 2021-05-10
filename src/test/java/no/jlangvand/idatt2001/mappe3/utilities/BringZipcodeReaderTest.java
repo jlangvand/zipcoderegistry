f2dd74a85a1fbcef58ec41e957d8f89e03da3bb9
Joakim Skogø Langvand <jlangvand@gmail.com>
2021-05-10 18:03:37 +0200

    Get rid of wildcard imports

--- src/test/java/no/jlangvand/idatt2001/mappe3/utilities/BringZipcodeReaderTest.java
+++ src/test/java/no/jlangvand/idatt2001/mappe3/utilities/BringZipcodeReaderTest.java
@@ -3,7 +3,8 @@ package no.jlangvand.idatt2001.mappe3.utilities;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.ThrowingSupplier;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class BringZipcodeReaderTest {
 
