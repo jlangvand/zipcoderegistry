133c30d3a7b1812d66270e4558fb7ff4fa367e00
Joakim Skogø Langvand <jlangvand@gmail.com>
2021-05-10 17:59:31 +0200

    Generalise zip code class

--- src/main/java/no/jlangvand/idatt2001/mappe3/model/Zipcode.java
+++ src/main/java/no/jlangvand/idatt2001/mappe3/model/Zipcode.java
@@ -1,42 +1,11 @@
 package no.jlangvand.idatt2001.mappe3.model;
 
-import no.jlangvand.idatt2001.mappe3.utilities.ZipType;
+import java.util.List;
 
-public class Zipcode {
+public abstract class Zipcode {
 
-  private final String code;
-  private final String area;
-  private final String municipalityCode;
-  private final String municipalityName;
-  private final ZipType type;
+  public abstract boolean anyParameterContains(CharSequence s);
 
-  public Zipcode(String zipcode, String area, String municipalityCode, String municipalityName,
-                 ZipType type) {
-    this.code = zipcode;
-    this.area = area;
-    this.municipalityCode = municipalityCode;
-    this.municipalityName = municipalityName;
-    this.type = type;
-  }
-
-  public String getCode() {
-    return code;
-  }
-
-  public String getArea() {
-    return area;
-  }
-
-  public String getMunicipalityCode() {
-    return municipalityCode;
-  }
-
-  public String getMunicipalityName() {
-    return municipalityName;
-  }
-
-  public ZipType getType() {
-    return type;
-  }
+  public abstract List<String> getLabels();
 
 }
--- src/main/java/no/jlangvand/idatt2001/mappe3/zipcodereader/ZipcodeReader.java
+++ src/main/java/no/jlangvand/idatt2001/mappe3/zipcodereader/ZipcodeReader.java
@@ -1,9 +1,9 @@
 package no.jlangvand.idatt2001.mappe3.zipcodereader;
 
-import no.jlangvand.idatt2001.mappe3.model.Zipcode;
+import no.jlangvand.idatt2001.mappe3.model.NorwegianZipcode;
 
 import java.util.List;
 
 public interface ZipcodeReader {
-  List<Zipcode> readAll() throws ZipCodeReaderException;
+  List<NorwegianZipcode> readAll() throws ZipCodeReaderException;
 }
