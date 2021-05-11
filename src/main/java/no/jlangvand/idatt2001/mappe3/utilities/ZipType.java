1467fd2af67d753c6ce9a5e6bbe08c9d57fe5e0a
Joakim Skogø Langvand <jlangvand@gmail.com>
2021-05-11 16:25:42 +0200

    Added description of ZIP code types

--- src/main/java/no/jlangvand/idatt2001/mappe3/utilities/ZipType.java
+++ src/main/java/no/jlangvand/idatt2001/mappe3/utilities/ZipType.java
@@ -1,10 +1,21 @@
 package no.jlangvand.idatt2001.mappe3.utilities;
 
 public enum ZipType {
-  ADDRESS,
-  BOX,
-  ADDRESS_AND_BOX,
-  SERVICE,
-  MULTI,
-  UNKNOWN
+  ADDRESS("Street Address"),
+  BOX("PO Box"),
+  ADDRESS_AND_BOX("Street Address and PO Box"),
+  SERVICE("Service number"),
+  MULTI("Multiple uses"),
+  UNKNOWN("Other/Unknown");
+
+  private final String label;
+
+  ZipType(String label) {
+    this.label = label;
+  }
+
+  @Override
+  public String toString() {
+    return label;
+  }
 }
