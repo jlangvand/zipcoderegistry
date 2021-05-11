01e39918e025fa340d39b74eeac481b85d8e1b6d
Joakim Skogø Langvand <jlangvand@gmail.com>
2021-05-11 15:56:17 +0200

    Set width of columns

--- src/main/java/no/jlangvand/idatt2001/mappe3/factories/TableFactory.java
+++ src/main/java/no/jlangvand/idatt2001/mappe3/factories/TableFactory.java
@@ -4,6 +4,7 @@ import javafx.collections.ObservableList;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.text.Text;
 import no.jlangvand.idatt2001.mappe3.model.Zipcode;
 import no.jlangvand.idatt2001.mappe3.utilities.StringUtils;
 
@@ -27,10 +28,16 @@ public class TableFactory {
     labels.forEach(label -> {
       var c = new TableColumn<T, String>(label);
       c.setCellValueFactory(new PropertyValueFactory<>(StringUtils.toPascalCase(label)));
+      c.setPrefWidth(calculateWidth(label));
       table.getColumns().add(c);
     });
     table.setItems(registry);
     return table;
+  }
+
+  // Not very elegant, but TableColumn lacks a (public) auto resize method
+  private static double calculateWidth(String s) {
+    return Math.max((new Text(s)).getLayoutBounds().getWidth() * 1.3, 60);
   }
 
 }
