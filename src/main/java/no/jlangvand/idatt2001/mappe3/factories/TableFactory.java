25df09d5963af2a879a5264faa36bd23666e3919
Joakim Skogø Langvand <jlangvand@gmail.com>
2021-05-13 10:42:19 +0200

    Make sure the getter returns a String, takes no parameters and is accessible

--- src/main/java/no/jlangvand/idatt2001/mappe3/factories/TableFactory.java
+++ src/main/java/no/jlangvand/idatt2001/mappe3/factories/TableFactory.java
@@ -6,7 +6,6 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.text.Text;
 
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.List;
@@ -29,23 +28,22 @@ public class TableFactory {
   /**
    * Get a TableView for an ObservableList of Zipcodes.
    *
-   * @param registry list of Zipcodes, must not be empty
-   * @param <T>      type of Zipcode, normally inferred from registry parameter
+   * @param list list of Zipcodes, must not be empty
+   * @param <T>  type of Zipcode, normally inferred from the list parameter
    * @return TableView of Zipcodes
+   * @throws RuntimeException if called with an empty list
    */
-  public static <T> TableView<T> getTableView(ObservableList<T> registry) {
-    var getters = getGettersFromObject(registry.stream().findFirst().orElseThrow());
+  public static <T> TableView<T> getTableView(ObservableList<T> list) {
+    var getters = getGettersFromObject(list.stream().findFirst().orElseThrow());
     var table = new TableView<T>();
     getters.forEach(getter -> {
-      if (!getter.getName().equals("getClass")) {
-        var name = getter.getName().substring(3);
-        var c = new TableColumn<T, String>(name);
-        c.setCellValueFactory(new PropertyValueFactory<>(name));
-        c.setPrefWidth(Math.max(calculateWidth(name), calculateWidth(registry, getter)));
-        table.getColumns().add(c);
-      }
+      var name = getter.getName().substring(3);
+      var c = new TableColumn<T, String>(name);
+      c.setCellValueFactory(new PropertyValueFactory<>(name));
+      c.setPrefWidth(Math.max(calculateWidth(name), calculateWidth(list, getter)));
+      table.getColumns().add(c);
     });
-    table.setItems(registry);
+    table.setItems(list);
     return table;
   }
 
@@ -57,7 +55,12 @@ public class TableFactory {
     var methods = Arrays.stream(
         o.getClass().getSuperclass().getDeclaredMethods()).collect(Collectors.toList());
     methods.addAll(Arrays.stream(o.getClass().getDeclaredMethods()).collect(Collectors.toList()));
-    methods.removeIf(method -> !method.getName().startsWith("get"));
+    // Remove methods that doesn't fit our expectations for a getter
+    methods.removeIf(method ->
+        !method.getName().startsWith("get")
+            || method.getReturnType() != String.class
+            || method.getParameterCount() > 0
+            || !method.canAccess(o));
     return methods;
   }
 
@@ -67,7 +70,7 @@ public class TableFactory {
     list.forEach(o -> {
       try {
         max[0] = Math.max(max[0], calculateWidth((String) getter.invoke(o)));
-      } catch (IllegalAccessException | InvocationTargetException | ClassCastException e) {
+      } catch (Exception ignored) {
         // This method is not critical, so we'll log the exception but otherwise ignore it
         LOGGER.log(INFO, () -> "Failed to invoke method %s".formatted(getter.getName()));
       }
@@ -77,7 +80,7 @@ public class TableFactory {
 
   // Not very elegant, but TableColumn lacks a (public) auto resize method
   private static double calculateWidth(String s) {
-    return Math.max((new Text(s)).getLayoutBounds().getWidth() * 1.3, 60);
+    return Math.max((new Text(s)).getLayoutBounds().getWidth() + 20, 60);
   }
 
 }
