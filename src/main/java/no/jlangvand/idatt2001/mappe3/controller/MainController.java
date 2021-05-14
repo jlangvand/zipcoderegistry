511ce4778425e9f5eef1657846bbd5c048301f01
Joakim Skogø Langvand <jlangvand@gmail.com>
2021-05-14 11:14:34 +0200

    Add copyright

--- src/main/java/no/jlangvand/idatt2001/mappe3/controller/MainController.java
+++ src/main/java/no/jlangvand/idatt2001/mappe3/controller/MainController.java
@@ -1,3 +1,21 @@
+/*
+ *     Copyright © 2021. Joakim Skogø Langvand <jlangvand@gmail.com>
+ *
+ *     This file is part of Zip Code Registry.
+ *
+ *     Zip Code Registry is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     Zip Code Registry is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with Zip Code Registry.  If not, see <https://www.gnu.org/licenses/>.
+ */
 package no.jlangvand.idatt2001.mappe3.controller;
 
 import javafx.collections.FXCollections;
