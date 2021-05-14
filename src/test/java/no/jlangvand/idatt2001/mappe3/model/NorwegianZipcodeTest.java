56e77b991564b54695c8e3c1e18c038c793968bb
Joakim Skogø Langvand <jlangvand@gmail.com>
2021-05-14 12:17:46 +0200

    Documentation and improvements of tests

--- src/test/java/no/jlangvand/idatt2001/mappe3/model/NorwegianZipcodeTest.java
+++ src/test/java/no/jlangvand/idatt2001/mappe3/model/NorwegianZipcodeTest.java
@@ -1,5 +1,24 @@
+/*
+ *     Copyright © 2021. Joakim Skogø Langvand <jlangvand@gmail.com>
+ *
+ *     This file is part of Zip Code Registry.
+ *
+ *     Zip Code Registry is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     Zip Code Registry is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with Zip Code Registry.  If not, see <https://www.gnu.org/licenses/>.
+ */
 package no.jlangvand.idatt2001.mappe3.model;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -10,6 +29,10 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 class NorwegianZipcodeTest {
 
   static NorwegianZipcode zipcode = new NorwegianZipcode("0123", "Oslo", "1234", "Oslo", "B");
+
+  /*
+   Test getters
+   */
 
   @Test
   void getMunicipalityCode() {
@@ -26,7 +49,11 @@ class NorwegianZipcodeTest {
     assertEquals("B", zipcode.getType());
   }
 
+  /*
+   Test the matching method used for searching.
+   */
   @Test
+  @DisplayName("Should return true if and only if a searchable parameter starts with string")
   void anyParameterContains() {
     assertTrue(zipcode.anyParameterContains("os"));
     assertTrue(zipcode.anyParameterContains("01"));
@@ -34,7 +61,13 @@ class NorwegianZipcodeTest {
     assertFalse(zipcode.anyParameterContains("23"));
   }
 
+  /*
+   * A Norwegian zip code consists of exactly four digits.
+   *
+   * The constructor should throw an IllegalArgumentException if other values are provided.
+   */
   @Test
+  @DisplayName("Test sanity check of zip code parameter")
   void testZipSanityCheck() {
     assertThrows(IllegalArgumentException.class, () ->
         new NorwegianZipcode("12345", "", "", "", ""));
--- src/test/java/no/jlangvand/idatt2001/mappe3/utilities/BringZipcodeReaderTest.java
+++ src/test/java/no/jlangvand/idatt2001/mappe3/utilities/BringZipcodeReaderTest.java
@@ -1,7 +1,28 @@
+/*
+ *     Copyright © 2021. Joakim Skogø Langvand <jlangvand@gmail.com>
+ *
+ *     This file is part of Zip Code Registry.
+ *
+ *     Zip Code Registry is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     Zip Code Registry is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with Zip Code Registry.  If not, see <https://www.gnu.org/licenses/>.
+ */
 package no.jlangvand.idatt2001.mappe3.utilities;
 
 import no.jlangvand.idatt2001.mappe3.zipcodereader.BringZipcodeReader;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.io.File;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -9,15 +30,35 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 class BringZipcodeReaderTest {
 
   private static final String REGISTRY_URL = "https://bit.ly/pnrreg";
+  private static final File REGISTRY_FILE = new File(
+      Thread.currentThread().getContextClassLoader().getResource("bringreg").getPath());
+
+  /*
+   The reader class will attempt to open a stream upon creation, thus it can be tested simply by
+   instantiating it.
+   */
 
   @Test
-  void readFromURL() {
+  @DisplayName("Instantiate a reader from URL, should not throw exception")
+  void testInstantiatingFromURL() {
     assertDoesNotThrow(() -> new BringZipcodeReader(REGISTRY_URL));
   }
 
   @Test
+  @DisplayName("Instantiate a reader from file, should not throw exception")
+  void testInstantiationFromFile() {
+    assertDoesNotThrow(() -> new BringZipcodeReader(REGISTRY_FILE));
+  }
+
+  /*
+   When reading from the registry file, it should be enough to check that the returned list is not
+   empty.
+   */
+
+  @Test
+  @DisplayName("Read registry from reader and check that the list is not empty")
   void readAll() {
-    assertTrue((new BringZipcodeReader(REGISTRY_URL)).readAll().size() > 1);
+    assertTrue((new BringZipcodeReader(REGISTRY_FILE)).readAll().size() > 1);
   }
 
 }
